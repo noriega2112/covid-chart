283579aa93e40aa62fb6e3dfcb798cfcb47277aa
Edwin Noriega <adolfonoriega9@gmail.com>
2020-04-27 00:23:54 -0600

    Updates

---
+++ precache-manifest.b254f5f4b7d3bc932bd3cf55de806556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0413ec59ea03eb7679be583d4a37b4d7",
+    "url": "/covid-chart/index.html"
+  },
+  {
+    "revision": "2e22821afa3b3630e925",
+    "url": "/covid-chart/static/css/main.6fbc2ad8.chunk.css"
+  },
+  {
+    "revision": "da04a8ec9feb7055c2e0",
+    "url": "/covid-chart/static/js/2.50c81ad7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-chart/static/js/2.50c81ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e22821afa3b3630e925",
+    "url": "/covid-chart/static/js/main.923d8c6c.chunk.js"
+  },
+  {
+    "revision": "9cf53c8cd00c191f5215",
+    "url": "/covid-chart/static/js/runtime-main.a9ca5678.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-chart/static/media/covid.d7265326.png"
+  }
+]);
